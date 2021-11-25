761dadd6f06567d8e79e6a7d940ec7c48221d6da
CMS999 <cauams113@gmail.com>
2021-11-25 18:24:31 -0300

    Final

--- App.js
+++ App.js
@@ -1,14 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, Linking, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ function App() {
+  const [Celular, setCelular] = useState('')
+  const [Mensagem, setMensagem] = useState('')
+  let options = 'pt-BR'
+  const dia = new Date().getDate()
+  const mes = new Date().getMonth()
+  const ano = new Date().getFullYear()
+  const hora = new Date().getHours()
+  const minutos = new Date().getMinutes()
+  let corminutos ='';
+
+  function Teste(){
+    if (minutos.toString().length<2) {
+      corminutos = "0"+minutos
+    }
+    const data = 'Hora: '+hora+':'+minutos+'\n'+'Data: '+dia+'/'+mes+'/'+ano
+    Linking.canOpenURL("whatsapp://send?text="+Mensagem).then(supported => {
+      return Linking.openURL(
+        "https://api.whatsapp.com/send?phone="+Celular+"&text="+data+"\n"+"Mensagem: "+Mensagem
+      );
+    })
+  }
+
+    return (
+      <View style={styles.container}>
+        <Text
+          style={styles.text}
+        >
+          Digite um número de celular (destinatário, exemplo: 15991112222) e a mensagem. 
+        </Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          maxLength={11}
+          placeholder={'Ensira o seu número de celular'}
+          onChangeText={(val)=>setCelular(55+val)} 
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={'Ensira a mensagem'}
+          onChangeText={(val)=>setMensagem(val)} 
+        />
+        <Button
+          title={"Enviar mensagem"}
+          onPress={() => Teste() }>
+          Enviar Mensagem
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+    );
+  
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +61,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  input:{
+    flex: 1,
+    width:300 ,
+    maxHeight:60,
+    padding: 10,
+    borderWidth:1,
+    borderColor:'black',
+    borderRadius:5,
+    marginBottom:5
+  },
+  text:{
+    marginBottom:15,
+    marginTop:50,
+    padding:10,
+    textAlign: 'center',
+  }
 });
+
+export default App;
